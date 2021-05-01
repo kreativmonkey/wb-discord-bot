081b815965a9c89d6161a57151a50763a51827ad
Sebastian Preisner <kreativmonkey@calyrium.org>
2021-05-01 19:11:54 +0200

    add guild name

--- bot.py
+++ bot.py
@@ -6,12 +6,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-	print('Wir sind eingeloggt als User {}'.format(client.user.name))
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )    
+    
+    channels = '\n - '.join([channel.name for channel in guild.channels])
+    print(f'Guild Channels:\n - {channels}')
 	
 	
 @client.event
@@ -23,5 +32,5 @@ async def on_message(message):
     # ToDo: Nur alle x Tage oder alle x Meldungen darauf reagieren...
 	if 'hzp' in message.content.lower():
 		await message.channel.send('**Wichtige Informationen** zur **HZP** findest du auch auf unserem Discourse unter: https://talk.wb-student.org/tag/hzp')
-
+    
 client.run(TOKEN)
