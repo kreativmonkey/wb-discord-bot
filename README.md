a64d5b02d6e146e9796bf2c8c84ab28fef1cad8f
Darksoul4626 <philvs94@outlook.com>
2021-05-03 22:47:36 +0200

    added readme and first try to commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Creating and appanding a bot to a discord server 
+To create a bot, open the following url 
+* https://discord.com/developers/applications  
+
+and create an application. The application name will be the botname. <br/>
+Take the **client_Id** of the bot and the required **permission level** and fill in the url below.
+
+To add a bot to an server open the following url with filled informations.
+* https://discord.com/api/oauth2/authorize?client_id=[XXX]&scope=bot&permissions=[permissionLevel]
+
+
+# Helpfull links
+* https://discord.com/developers/applications 
+* https://support.discord.com/hc/en-us/articles/360045093012
+* https://discord.com/developers/docs/topics/oauth2#bots 
+
+## Discord Python
+**Examples**
+* https://github.com/Rapptz/discord.py/blob/master/examples/basic_bot.py
+
+**Read the fucking manual**
+* https://discordpy.readthedocs.io/en/latest/index.html 
+* https://discordpy.readthedocs.io/en/latest/api.html#utility-functions 
+
+**How to make a bot**
+* https://www.writebots.com/how-to-make-a-discord-bot/
--- bot.py
+++ bot.py
@@ -1,6 +1,4 @@
 import os
-
-
 import discord
 from dotenv import load_dotenv
 
@@ -10,27 +8,33 @@ GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-    guild = discord.utils.get(client.guilds, name=GUILD)
-    
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )    
-    
-    channels = '\n - '.join([channel.name for channel in guild.channels])
-    print(f'Guild Channels:\n - {channels}')
-	
-	
+    print("Bot is ready!")
+
+    # ps: did not know what to take as guild-name parameter
+    # guild = discord.utils.get(client.guilds, name=GUILD)
+    guild = discord.utils.get(client.guilds)
+
+    if guild:
+        print(
+            f'{client.user} is connected to the following guild:\n'
+            f'{guild.name}(id: {guild.id})'
+        )
+
+        channels = '\n - '.join([channel.name for channel in guild.channels])
+        print(f'Guild Channels:\n - {channels}')
+
+
 @client.event
 async def on_message(message):
-	if message.author.bot:
-		return
-		
+    if message.author.bot:
+        return
+
     # Reagiere auf das kürzel HZP um Informationen für die Hochschulzugangsprüfung an zu zeigen.
     # ToDo: Nur alle x Tage oder alle x Meldungen darauf reagieren...
-	if 'hzp' in message.content.lower():
-		await message.channel.send('**Wichtige Informationen** zur **HZP** findest du auch auf unserem Discourse unter: https://talk.wb-student.org/tag/hzp')
-    
-client.run(TOKEN)
+    if 'hzp' in message.content.lower():
+        await message.channel.send('**Wichtige Informationen** zur **HZP** findest du auch auf unserem Discourse unter: https://talk.wb-student.org/tag/hzp')
+
+client.run(TOKEN, bot=True)
