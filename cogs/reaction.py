4fa4780c6cf00d4beb90346fc514b17a8df065f8
Darksoul4626 <philvs94@outlook.com>
2021-05-06 08:46:17 +0200

    fix #7: send info-message for some keywords only if the info-message not send in the last 10 days and 25 messages before.

--- cogs/reaction.py
+++ cogs/reaction.py
@@ -3,6 +3,7 @@ import discord
 import requests
 import json
 import re  # regex
+from datetime import datetime
 
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -19,6 +20,8 @@ class Reaction(commands.Cog):
 
     def __init__(self, client):
         self.client = client
+        print('init')
+        self.last_keyword_message = []
 
     # Events
     @commands.Cog.listener()
@@ -26,10 +29,21 @@ class Reaction(commands.Cog):
         if message.author.bot:
             return
 
-        # Reagiere auf das kürzel HZP um Informationen für die Hochschulzugangsprüfung an zu zeigen.
-        # ToDo: Nur alle x Tage oder alle x Meldungen darauf reagieren...
-        if 'hzp' in message.content.lower():
-            await message.channel.send('**Wichtige Informationen** zur **HZP** findest du auch auf unserem Discourse unter: https://talk.wb-student.org/tag/hzp')
+        # react on keywords related to 'hzp' to show some informations about.
+        hzpKeywords = ('hzp', 'hochschulzugangsprüfung', 'zugangsprüfung')
+
+        if any(keyWord in message.content.lower() for keyWord in hzpKeywords):
+            # checks for last 25 messages, whether the bot already send this message in the last 10 days
+            if await self.messageAlreadySendBefore(message.channel, '**Wichtige Informationen** zur **HZP**'):
+                await message.channel.send('**Wichtige Informationen** zur **HZP** findest du auch auf unserem Discourse unter: https://talk.wb-student.org/tag/hzp')
+
+    # checks channels last 25 messages whether the bot already send a messages, starts with <messageStart>, in the last 10 days
+    async def messageAlreadySendBefore(self, channel, messageStart):
+        async for message in channel.history(limit=25):
+            if message.author.bot and message.content.startswith(messageStart) and (datetime.now() - message.created_at).days < 10:
+                return False
+
+        return True
 
     # search for a given value in the discourse forum under the BASEURL.
     # to search for posts with tags the searchValue can be like 'tag:sei,bsra'
