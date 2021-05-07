ddd75649243e97680a0e6ab4559272c7b6664131
Sebastian Preisner <kreativmonkey@calyrium.org>
2021-05-07 13:17:39 +0200

    restrict permission close #14

--- bot.py
+++ bot.py
@@ -14,14 +14,17 @@ HOOKTOKEN = os.getenv('WEBHOOK_TOKEN')
 client = commands.Bot(command_prefix = '/')
 
 @client.command()
+@commands.has_permissions(administrator=True)
 async def load(ctx, extension):
     client.load_extension(f'cogs.{extension}')
 
 @client.command()
+@commands.has_permissions(administrator=True)
 async def unload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
 
 @client.command()
+@commands.has_permissions(administrator=True)
 async def reload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
     client.load_extension(f'cogs.{extension}')
